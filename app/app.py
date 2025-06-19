c9d4e8beea77b2df924a90d8481fb53e2d8217e2
Shivam Sinha <shivamsinha2000@gmail.com>
2025-06-19 16:07:02 -0400

    Initial Commit

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask app with Jenkins & Docker on AWS!"
+    return "Hello from Flask app with Jenkins & Docker on AWS AUtomation wORKING!"
 
 @app.route("/health")
 def health():
